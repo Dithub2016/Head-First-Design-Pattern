8983f8d53a2375814df4554961389335e3e93447
lo <dluo2017@163.com>
2019-01-18 13:53:12 +0800

    优化多入口

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,22 @@
 const path = require('path');
+const glob = require('glob');
+
+const entry = () => new Promise(resolve => {
+    glob('src/*.ts', (err, files) => {
+        resolve(files.reduce((result, file) => {
+            const fileName = /src\/(.+)\.ts/.exec(file)[1];
+            result[fileName] = `./${fileName}.ts`;
+            return result;
+        }, {}));
+    });
+});
+
 
 module.exports = {
     mode: 'development',
     devtool: 'inline-source-map',
     context: path.resolve(__dirname, 'src'),
-    entry: {
-        Duck: './Duck.ts',
-        Character: './Character.ts',
-    },
+    entry,
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: '[name].js',
